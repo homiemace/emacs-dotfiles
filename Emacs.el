da148d937d85f97e0b01a13c69fa406ee1e46431
homie <90294477+homiemace@users.noreply.github.com>
2025-11-17 23:14:36 -0600

    feat: add LSP support for TypeScript

--- Emacs.el
+++ Emacs.el
@@ -186,6 +186,31 @@
 (use-package magit
   :bind ("C-x g" . magit-status))
 
+(defun efs/lsp-mode-setup ()
+  (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
+  (lsp-headerline-breadcrumb-mode))
+
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :hook (lsp-mode . efs/lsp-mode-setup)
+  :init
+  (setq lsp-keymap-prefix "C-c l")  ;; Or 'C-l', 's-l'
+  :config
+  (lsp-enable-which-key-integration t))
+
+(use-package lsp-ui
+  :hook (lsp-mode . lsp-ui-mode)
+  :custom
+  (lsp-ui-doc-position 'bottom))
+
+(use-package lsp-ivy)
+
+(use-package typescript-mode
+  :mode "\\.ts\\'"
+  :hook (typescript-mode . lsp-deferred)
+  :config
+  (setq typescript-indent-level 2))
+
 (use-package projectile
   :init (projectile-mode +1)
   :bind (:map projectile-mode-map
--- init.el
+++ init.el
@@ -1,4 +1,9 @@
 ;;; init.el --- Bootstrap Org Configuration -*- lexical-binding: t -*-
 
-;; Load the org config file
+;; Separate Custom changes from main config
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
+
+(require 'org)
 (org-babel-load-file (expand-file-name "Emacs.org" user-emacs-directory))
