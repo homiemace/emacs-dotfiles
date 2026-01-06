1d6c66a318c863e4ff0a9e98964aa2d299ff8bf9
homie <90294477+homiemace@users.noreply.github.com>
2026-01-05 22:00:45 -0600

    feat: add gptel package

--- custom.el
+++ custom.el
@@ -4,7 +4,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(package-selected-packages
+   '(auto-package-update catppuccin-theme company counsel dashboard
+			 doom-modeline exec-path-from-shell gptel
+			 helpful ivy-prescient ivy-rich lsp-ivy
+			 lsp-tailwindcss lsp-ui magit no-littering
+			 org-bullets page-break-lines projectile
+			 rainbow-delimiters svelte-mode
+			 typescript-mode visual-fill-column)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
