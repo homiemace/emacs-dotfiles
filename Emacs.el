c8c95cb4d9f5725134a2111ef06a364b6672adbe
homie <90294477+homiemace@users.noreply.github.com>
2026-01-26 18:28:26 -0600

    feat: switch gptel backend from OpenRouter to Ollama

--- Emacs.el
+++ Emacs.el
@@ -209,17 +209,10 @@
 	gptel-use-tools t)
 :config
 (setq gptel-backend
-      (gptel-make-openai "OpenRouter"
-        :host "openrouter.ai"
-        :endpoint "/api/v1/chat/completions"
+      (gptel-make-openai "Ollama"
+        :host "localhost:11434"
         :stream t
-        :key gptel-api-key
-        :models '(openai/gpt-3.5-turbo
-                  mistralai/mixtral-8x7b-instruct
-                  meta-llama/codellama-34b-instruct
-                  codellama/codellama-70b-instruct
-                  google/palm-2-codechat-bison-32k
-                  google/gemini-pro)
+        :models '(mistral:latest)
         :request-params '(:transforms ["middle-out"]))))
 
 (use-package gptel-agent
